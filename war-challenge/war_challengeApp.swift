8c3d68dedd6e951533201854c2fa65b0bfbc7620
Vy Nguyen <vydpnguyen@gmail.com>
2022-10-22 19:36:02 -0700

    Initial Commit

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by  NGUYEN DANG PHUONG VY on 7/10/22.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
